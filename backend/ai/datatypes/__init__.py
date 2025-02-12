6bfeebc11b4d90364fb8c9f812c1ac626a3ab54f
Gyokhan Kochmarla <gokhankocmarli@gmail.com>
2025-02-12 23:25:16 +0200

    Revert "[WIP] Implement the most basic version using Ollama"

--- backend/ai/datatypes/__init__.py
+++
@@ -1,9 +0,0 @@
-"""This package provides structured outputs for the LLM assistants."""
-
-from backend.ai.datatypes.ocr_response import OcrResponse
-from backend.ai.datatypes.ocr_response import OcrStatus
-
-__all__ = [
-    "OcrResponse",
-    "OcrStatus",
-]
